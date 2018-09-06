cb378a628da1be67f0c2d574766ca44fcaec5fab
Hentioe <me@bluerain.io>
2018-09-07 02:31:37 +0800

    Add some trait

--- src/upstms/mod.rs
+++ src/upstms/mod.rs
@@ -1,1 +1,10 @@
 pub mod text_image;
+
+pub mod upstms {
+
+    pub trait Upstream {
+        fn gen_text(path: &str, width: u32) -> Result<&'static str, &'static str>;
+        fn gen_html(path: &str, width: u32, scale: f32) -> Result<&'static str, &'static str>;
+        fn output(path: &str, width: u32) -> Result<&'static str, &'static str>;
+    }
+}
